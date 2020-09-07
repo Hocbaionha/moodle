a29cb0d09e5fc0c8fef9e8f930fa424b08f00f90
Ngoc Anh <anhlucky18@gmail.com>
2020-09-07 13:45:44 +0700

    fix front menu

---
+++ theme/classon/renderers.php
@@ -0,0 +1,32 @@
+<?PHP
+class theme_classon_core_renderer extends core_renderer {
+
+
+    public function custom_menu_frontend($custommenuitems = '') {
+        global $CFG;
+
+        if (empty($custommenuitems) && !empty($CFG->custommenuitems)) {
+            $custommenuitems = $CFG->custommenuitems;
+        }
+        $custommenu = new custom_menu($custommenuitems, current_language());
+        return $this->render_custom_menu_frontend($custommenu);
+    }
+    protected function render_custom_menu_frontend(custom_menu $menu) {
+//        global $CFG;
+
+//        $langs = get_string_manager()->get_list_of_translations();
+        $haslangmenu = $this->lang_menu() != '';
+
+        if (!$menu->has_children() && !$haslangmenu) {
+            return '';
+        }
+
+        $content = '';
+        foreach ($menu->get_children() as $item) {
+            $context = $item->export_for_template($this);
+            $content .= $this->render_from_template('core/custom_menu_item', $context);
+        }
+
+        return $content;
+    }
+}
