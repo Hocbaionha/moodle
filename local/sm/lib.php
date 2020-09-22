87c46c9260d2964827df635beac7fc396161d3ff
Ngoc Anh <anhlucky18@gmail.com>
2020-09-22 18:30:16 +0700

    fix timestamp

--- local/sm/lib.php
+++ local/sm/lib.php
@@ -1,6 +1,7 @@
 <?PHP 
 require  dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 use Google\Cloud\Firestore\FirestoreClient;
+use Google\Cloud\Core\Timestamp;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -18,19 +19,18 @@ function local_sm_enrole(core\event\user_loggedin $event){
     //check student
 
     $student_role = $DB->get_record("role",array("shortname"=>"student"))->id;
-    $docRef = $db->collection('students')->document($uid);
-    $snapshot = $docRef->snapshot();
-    if ($snapshot->exists()) {
-        $data = $snapshot->data();
-        $products =  $data['products'];
-        //each product (thẻ thành viên)
-        foreach($products as $product){
+    $documents = $db->collection('students')->document($uid)->collection('products')->documents();
+    foreach ($documents as $document) {
+        if ($document->exists()) {
+            
+            $product = $document->data();
             $courses = $product['courses'];
             $endtime=$product['end_time']->get()->getTimestamp();
-            
             foreach($courses as $course) {
-                check_enrol($course,$USER->id,$student_role,$endtime);
+                check_enrol($course["shortname"],$USER->id,$student_role,$endtime);
             }
+        } else {
+            printf('Document %s does not exist!');
         }
     }
 }
@@ -75,7 +75,7 @@ function local_sm_attempt_submitted(mod_quiz\event\attempt_submitted $event) {
     $quiz = $event->get_record_snapshot('quiz', $quiz_attempt->quiz);
     //get course data
     $course = $event->get_record_snapshot('course', $event->courseid);
-    
+    // firebase.firestore.Timestamp.fromDate(data.birthdate.toDate())
     $send_data = [];
     $send_data['uid'] = $USER->uid;
     $send_data['course'] = $course->shortname ;
@@ -85,14 +85,16 @@ function local_sm_attempt_submitted(mod_quiz\event\attempt_submitted $event) {
     $send_data['quiz_name'] = $quiz->name;
     $send_data['cmid'] = $quiz->cmid;
     $send_data['quiz_attempt_id'] = $quiz_attempt->id;
-    $send_data['timestart'] = $quiz_attempt->timestart;
-    $send_data['timefinish'] = $quiz_attempt->timefinish;
-    $send_data['timemodified'] = $quiz_attempt->timemodified;
-    $send_data['sumgrades'] = $quiz->sumgrades;
-    $send_data['grade'] = $quiz->grade;
-    $send_data['url'] = (string)$event->get_url();
+    $date = new DateTime();
+    $send_data['timestart'] = new Timestamp($date->setTimestamp($quiz_attempt->timestart));
+    $send_data['timefinish'] = new Timestamp($date->setTimestamp($quiz_attempt->timefinish));
+    $send_data['timemodified'] = new Timestamp($date->setTimestamp($quiz_attempt->timemodified));
+    $send_data['sumgrades'] = (int)$quiz_attempt->sumgrades;
+    $send_data['grade'] = (int)$quiz_attempt->sumgrades/(int)$quiz->sumgrades*(int)$quiz->grade;
+    $send_data['url'] = ((string)$event->get_url())."&cmid=".$quiz->cmid;
     
     $db = new FirestoreClient($CFG->firebase_config);
+
     $db->collection('students')->document($USER->uid)->collection('activities')->newDocument()->set($send_data);
     return true;
     }catch (Exception $exception){
