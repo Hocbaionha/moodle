43faad1adc53c32f75fd4cf48f03da0710e40704
Ngoc Anh <anhlucky18@gmail.com>
2020-10-30 17:56:47 +0700

    fix popup saving

--- local/sm/course_popup.php
+++ local/sm/course_popup.php
@@ -9,10 +9,14 @@ if(!isset($SESSION->wantsurl)){
 if($USER->id>2){
     //not admin or guest
     $sql = "select u.id,u.username,ud.data from mdl_user u join mdl_user_info_data ud on ud.userid=u.id
-    join mdl_user_info_field uf on uf.id=ud.fieldid where u.id=? and uf.shortname='phone' and ud.data is not null and ud.data !=''";
+    join mdl_user_info_field uf on uf.id=ud.fieldid where u.id=? and uf.shortname='phone' ";
     $check = $DB->get_record_sql($sql,array("id"=>$USER->id));
     if(!$check){
         $sql = "insert into mdl_user_info_data (userid,fieldid,data) values(?,(select id from mdl_user_info_field where shortname='phone'),?)";
+        $DB->execute($sql,array("userid"=>$USER->id,"data"=>$phone));
+        echo "done";
+    } else {
+        $sql = "update mdl_user_info_data set data=? where u.id=? and fieldid=(select id from mdl_user_info_field where shortname='phone')";
         $DB->execute($sql,array("userid"=>$USER->id,"data"=>$phone));
         echo "done";
     }
