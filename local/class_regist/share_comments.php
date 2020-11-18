0007ac82e582ed9f67f7498c3912a30a468631ed
cuongpt <phancuongcdth12@gmail.com>
2020-11-18 15:17:28 +0700

    update view comments EEC Trial

---
+++ local/class_regist/share_comments.php
@@ -0,0 +1,155 @@
+<style>
+    body {
+        margin-top: 20px;
+        color: #1a202c;
+        text-align: left;
+        background-color: #e2e8f0;
+    }
+
+    .main-body {
+        padding: 15px;
+    }
+
+    .card {
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
+    }
+
+    .card {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        min-width: 0;
+        word-wrap: break-word;
+        background-color: #fff;
+        background-clip: border-box;
+        border: 0 solid rgba(0, 0, 0, .125);
+        border-radius: .25rem;
+    }
+
+    .card-body {
+        flex: 1 1 auto;
+        min-height: 1px;
+        padding: 1rem;
+    }
+
+    .gutters-sm {
+        margin-right: -8px;
+        margin-left: -8px;
+    }
+
+    .gutters-sm > .col, .gutters-sm > [class*=col-] {
+        padding-right: 8px;
+        padding-left: 8px;
+    }
+
+    .mb-3, .my-3 {
+        margin-bottom: 1rem !important;
+    }
+
+    .bg-gray-300 {
+        background-color: #e2e8f0;
+    }
+
+    .h-100 {
+        height: 100% !important;
+    }
+
+    .shadow-none {
+        box-shadow: none !important;
+    }
+
+    .text-secondary {
+        color: #0b0b0b !important;
+        font-size: 14pt;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .page-header-headings h1 {
+            font-size: 14px;
+            font-weight: bold;
+        }
+    }
+
+    h3, h4, h5, h6 {
+        font-weight: bold;
+    }
+
+    pre {
+        white-space: pre-wrap;
+        white-space: -moz-pre-wrap;
+        white-space: -pre-wrap;
+        white-space: -o-pre-wrap;
+        word-wrap: break-word;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    }
+
+    #page-header {
+        display: none;
+    }
+</style>
+<div>
+
+    <?php
+    if (!empty($member)) {
+        foreach ($member as $object) {
+            echo '<div class="container">
+        <div class="main-body">
+            <div class="row gutters-sm">
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex flex-column align-items-center text-center">
+                                <img src="'.$CFG->wwwroot.'/theme/classon/pix/EnglishEveryday_Club.png"
+                                     alt="Admin" class="rounded" width="250">
+                                <div class="mt-3">
+                                    <h4>ENGLISH CLUB EVERYDAY</h4>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h5 class="mb-0">Họ và tên</h5>
+                                </div>
+                                <div class="col-md-6 text-secondary">
+                                    '.$object->name.'
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h5 class="mb-0">Số điện thoại</h5>
+                                </div>
+                                <div class="col-md-6 text-secondary ">
+                                     '.$object->phone.'
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="d-flex flex-column align-items-center text-center">
+                                <a href="'.$CFG->wwwroot.'/local/hbon_payment/index.php?product_id=17">
+                                    <button class="btn btn-outline-primary">ĐĂNG KÍ HỌC</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="col-md-12 text-center">
+                                <h4 class="mb-0">Nhận xét của giáo viên :</h4>
+                            </div>
+                            <hr>
+                            <div class="text-secondary">
+                                <pre>'.$object->comments.'</pre>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';
+        }
+    }
+    ?>
+</div>
+
--- local/class_regist/share_view.php
+++ local/class_regist/share_view.php
@@ -1,5 +1,7 @@
 <?php
 
+use block_xp\di;
+
 require(__DIR__ . '/../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
 global $USER,$CFG,$DB,$PAGE;
@@ -17,13 +19,14 @@ $phone = optional_param('phone', 0, PARAM_TEXT);
 try{
     $check = $DB->count_records('hbon_classes_register',array('phone'=>$phone));
     if($check>0){
-        $member = $DB->get_record('hbon_classes_register', array('phone'=>$phone));
-        $class= $DB->get_record('hbon_classes', array('id'=>$member->classid));
-        $templatecontext = [
-            'hbon_classes_register'=>$member,
-            'class'=> $class->code
-        ];
-        echo $OUTPUT->render_from_template('theme_classon/share_comments', $templatecontext);
+        $member = $DB->get_records('hbon_classes_register', array('phone'=>$phone));
+//        $class= $DB->get_record('hbon_classes', array('id'=>$member->classid));
+//        $templatecontext = [
+//            'hbon_classes_register'=>$member,
+////            'class'=> $class->code
+//        ];
+        require_once(__DIR__ . '/share_comments.php');
+//        echo $OUTPUT->render_from_template('theme_classon/share_comments', $templatecontext);
     }else{
 
     }
