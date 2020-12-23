02c7c70e6b1bb6d62323a23f42aafd843f58be4a
cuongpt <phancuongcdth12@gmail.com>
2020-12-23 11:28:57 +0700

    update add close connection end send now

--- local/sm/lib.php
+++ local/sm/lib.php
@@ -359,6 +359,20 @@ function local_sm_attempt_submitted(mod_quiz\event\attempt_submitted $event)
         $firestore = $factory->createFirestore();
         $db = $firestore->database();
 
+        ob_end_clean();
+        header("Connection: close");
+        ignore_user_abort(); // optional
+        ob_start();
+        echo ('Text the user will see');
+        $size = ob_get_length();
+        header("Content-Length: $size");
+        ob_end_flush(); // Strange behaviour, will not work
+        flush();            // Unless both are called !
+        session_write_close(); // Added a line suggested in the comment
+        // Do processing here
+        sleep(30);
+        echo('Text user will never see');
+
         $db->collection('students')->document($USER->uid)->collection('grades')->newDocument()->set($send_data);
         $complete_activities = [];
         $complete_activities ['activity_id'] = (int)$quiz->cmid;
