ebcd69f6a360e72b9725b51b4b3588f7e3fffecb
cuongpt <phancuongcdth12@gmail.com>
2021-04-01 17:14:33 +0700

    update edit link_live popup automation

---
+++ local/sm/popup_ext.php
@@ -0,0 +1,48 @@
+<?PHP
+
+require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+require_once(dirname(dirname(__DIR__)) . '/config.php');
+require_once($CFG->dirroot . "/lib/externallib.php");
+global $CFG, $USER, $SESSION, $DB;
+
+$popup_id = optional_param('popup_id', 0, PARAM_INT);
+$link_live = optional_param('link_live', '', PARAM_TEXT);
+$security = optional_param('security', '', PARAM_TEXT);
+
+if (isset($security) && $security ==="hocbaieditor"  && $popup_id !== 0 && filter_var($link_live, FILTER_VALIDATE_URL)) {
+    $res = [
+        "status"=> 200,
+        "exe_code"=>0,
+        "message"=>"update failed"
+    ];
+    $checkexist = $DB->count_records('hbon_popup_home', array('id' => $popup_id));
+    if ($checkexist !== 0) {
+        $old = $DB->get_record('hbon_popup_home', array('id' =>  $popup_id));
+        $newData = array(
+            'id' => (int)$old->id,
+            'title'=>$old->title,
+            'image'=>$old->image,
+            'link'=>$link_live,
+            'status'=>$old->status,
+            'created_at'=>$old->created_at,
+            'public_at'=>$old->public_at,
+            'expitime'=>$old->expitime,
+            'is_countdown'=>$old->is_countdown,
+            'to_course'=>$old->to_course,
+            'replay'=>$old->replay,
+            );
+        $result = $DB->update_record('hbon_popup_home', (object)$newData);
+        if($result == true){
+            echo  json_encode([
+                "status"=> 200,
+                "exe_code"=>1,
+                "message"=>"update success"
+            ]);
+        }
+    }else{
+        echo  json_encode($res);
+    }
+}
+
+
+
